78e9017b7cc284946e28464a20acde26fa058c8c
Voytas Yuriy <yuriy.voytas@edvantis.com>
2016-03-14 18:36:05 +0200

    first step, created project with yoman

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name hospitalJsApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the hospitalJsApp
+ */
+angular.module('hospitalJsApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name hospitalJsApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the hospitalJsApp
+ */
+angular.module('hospitalJsApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
